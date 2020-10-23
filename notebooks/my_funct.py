7ce0e2a0d1de06e3f1e532de5bbc128a63ae9a44
Jon Hickey <jon_hv123@yahoo.com>
2020-10-23 18:39:58 -0400

    final notebook included

--- notebooks/my_funct.py
+++ notebooks/my_funct.py
@@ -17,8 +17,9 @@ from sklearn.metrics import accuracy_score
 
 from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
 
-def def confusion_matrix_info(model, X_test, y_test):
+def confusion_matrix_info(model, X_test, y_test, save_path=None):
     '''
     Creates a confusion matrix for a given model
 
@@ -34,18 +35,17 @@ def def confusion_matrix_info(model, X_test, y_test):
     -------
     A confusion matrix of given model
     '''
+    fig, axes = plt.subplots(figsize=(10,5))
 
-    plot_confusion_matrix(model, X_test, y_test, ax=axes, cmap='prism', display_labels=y_tick_marks);
+    axes.set_title("Model Validation")
+    x_tick_marks = ['Predicted to Stay', 'Predicted to Churn']
+    y_tick_marks = ['Stayed', 'Churned']
+    
+    plot_confusion_matrix(model, X_test, y_test, ax=axes, cmap='prism', display_labels=y_tick_marks)
 
-    fig, axes = plt.subplots(figsize=(10,5));
-
-    axes.set_title("Model Validation");
-    x_tick_marks = ['Predicted to Stay', 'Predicted to Churn'];
-    y_tick_marks = ['Stayed', 'Churned'];
-
-    plt.xticks([0,1], x_tick_marks);
-    axes.set_xlabel('');
-    axes.set_ylabel('');
+    plt.xticks([0,1], x_tick_marks)
+    axes.set_xlabel('')
+    axes.set_ylabel('')
 
     SMALL_SIZE = 8
     MEDIUM_SIZE = 10
@@ -61,12 +61,70 @@ def def confusion_matrix_info(model, X_test, y_test):
     plt.tight_layout()
     confusion_matrix = plt.show()
 
-    return confusion_matrix
+    if save_path:
+        plt.savefig(save_path, transparent=True)
+
+    return confusion_matrix, fig
 
 
+def load_clean_data(df):
+    '''
+    Uses a dictionary to turn True/False and Yes/No to 0's and 1's
+
+    Parameters
+    ----------
+    df: Pandas DataFrame
+
+    Returns
+    -------
+    Data set with True/False and Yes/No changed to 0's and 1's
+    '''
+        
+    
+    churn_dict = {False: 0, True: 1},
+    yes_no_dict = {'no': 0, 'yes': 1},
+    df['churn'].replace(churn_dict, inplace=True),
+    df['international plan'].replace(yes_no_dict, inplace=True),
+    df['voice mail plan'].replace(yes_no_dict, inplace=True)
+
+    return df
 
 
-
+def create_plot_of_feature_importances(coeff_dict, top_num=10, figsize=(7, 6), title='Feature Importances', xlabel='Feature', ylabel='Correlation Percentage', append_title='', prepend_title='', width=0.8, xrot=0, bar_colors=None, percent=None, save_path=None):
+    ''' 
+    Inputs: 
+    
+    model: A trained ensemble model instance
+    X: a dataframe of the features used to train the model
+    '''
+    dict_keys = [key for key in coeff_dict.keys()]
+    dict_values = [abs(value) for value in coeff_dict.values()]
+    
+    coeff_dict = {dict_keys[x]: dict_values[x] for x in range(len(dict_values))}
+    coeff_dict = dict(list(coeff_dict.items())[:])
+    features_and_importances = zip(coeff_dict.keys(), coeff_dict.values())
+    features_and_importances = sorted(features_and_importances, key = lambda x: x[1], reverse=True)
+    
+    features = [i[0].title() for i in features_and_importances[1:top_num+1]]
+    print(features)
+    importances = [abs(i[1])*100 for i in features_and_importances[1:top_num+1]]
+    print(importances)
+    plt.rcdefaults()
+    fig, ax = plt.subplots(figsize=figsize)
+#     width = .35
+    plt.bar(features, importances, width=width, color=bar_colors)
+    if percent:
+        ax.yaxis.set_major_formatter(PercentFormatter())
+#     plt.gca().invert_yaxis()
+    title = ' '.join([prepend_title, title, append_title])
+    plt.title(title)
+    plt.xlabel('Feature', fontsize=13)
+    plt.ylabel(ylabel, fontsize=13)
+    plt.xticks(rotation=xrot)
+    plt.axis('tight')
+    plt.tight_layout()
+    if save_path:
+        plt.savefig(save_path, transparent=True)    
 
 
 
